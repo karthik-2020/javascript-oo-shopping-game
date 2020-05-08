807c63343e53ec83bdbcc8320993cdb79ee4563e
karthik-2020 <64995106+karthik-2020@users.noreply.github.com>
2020-05-07 22:36:48 -0400

    Completed module

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -230,10 +230,16 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function(Product) {
+        Product.id == id;
+    }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random() * 20) + 1;
+};
 
 
 const getProduct = (prodList, pId) => {
